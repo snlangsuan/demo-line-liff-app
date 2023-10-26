eca7cee272e0a499ea5be75745806749e66a23cf
snlangsuan <snlangsuan@users.noreply.github.com>
2023-10-26 03:13:31 +0000

    deploy: 27253ba443bbeb0f3a254d141406c7edc7abda01

--- _nuxt/static/1698288749/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdemo-line-liff-app\u002F_nuxt\u002Fstatic\u002F1698288749",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fdemo-line-liff-app\u002F",assetsPath:"\u002Fdemo-line-liff-app\u002F_nuxt\u002F",cdnURL:a}}}}(null));
